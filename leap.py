640c7830c8440db67d141057974f57fe36e83a7c
gopi301298 <46928595+gopi301298@users.noreply.github.com>
2019-01-25 11:56:10 +0000

    Create leap.py

---
+++ leap.py
@@ -0,0 +1,13 @@
+#gopi
+x=int(input())
+if x%4==0:
+  if x%100==0:
+     if x%400==0:
+         print("yes")
+     else:
+         print("No")
+  else: 
+       print("yes")
+else:
+    print("no")
+          
